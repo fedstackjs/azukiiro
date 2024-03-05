264a85938c3fb875c40c9fd7929866450a25a6e3
thezzisu <thezzisu@gmail.com>
2024-03-05 19:04:10 +0800

    chore: follow aoi update

--- common/solution.go
+++ common/solution.go
@@ -1,10 +1,11 @@
 package common
 
 type SolutionDetailsTest struct {
-	Name    string  `json:"name"`
-	Score   float64 `json:"score"`
-	Status  string  `json:"status"`
-	Summary string  `json:"summary"`
+	Name       string  `json:"name"`
+	Score      float64 `json:"score"`
+	ScoreScale float64 `json:"scoreScale"`
+	Status     string  `json:"status"`
+	Summary    string  `json:"summary"`
 }
 
 type SolutionDetailsJob struct {
